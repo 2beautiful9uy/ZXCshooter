22109a8e8ad6adafa009fb8f5a03be8c6d199631
2beautiful9uy <zxc63635@naver.com>
2015-11-01 14:44:13 +0900

    ZXCshooter
    different from ZXCkalista
    Dragon E active

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ZXCshooter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZXCaddons")]
+[assembly: AssemblyCopyright("Copyright ©  2000")]
+[assembly: AssemblyTrademark("Fappa")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.0.1.5")]
+[assembly: AssemblyFileVersion("2.0.1.5")]
